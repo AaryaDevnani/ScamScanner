64f35b664b23f8a4b5389404d22f2350e3d44068
aaryadevnani <aarya_devnani@hotmail.com>
2023-02-16 23:06:18 +0530

    Added IG bot detection to API and boilerplate for Chrome Extension

--- Server/app.py
+++ Server/app.py
@@ -6,23 +6,42 @@ import pandas as pd
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-@app.route('/ping', methods=['GET', 'POST'])
+@app.route('/ping', methods=['GET'])
 def index():
     return jsonify({"message": "pong"})
 
-@app.route('/bot', methods=['GET','POST'])
-def bot():
+@app.route('/twt-bot', methods=['POST'])
+def twtbot():
     if request.method == "POST":
         obj = json.loads(request.data)
+        print(obj)
         df = pd.DataFrame.from_dict([obj])
-        output = model.predict(df)
+        output = twt_model.predict(df)
         if(output == 1):
             Prediction = "Bot Account"
         elif output == 0:
             Prediction = "Human"
+    print(Prediction)
+    return jsonify({"Prediction": Prediction})
+
+@app.route('/ig-bot', methods=['POST'])
+def igbot():
+    if request.method == "POST":
+        obj = json.loads(request.data)
+        print(obj)
+        df = pd.DataFrame.from_dict([obj])
+        output = ig_model.predict(df)
+        if(output == 1):
+            Prediction = "Bot Account"
+        elif output == 0:
+            Prediction = "Human"
+    print(Prediction)
     return jsonify({"Prediction": Prediction})
 
 if __name__ == '__main__':
     with open('model_pkl', 'rb') as f:
-        model = pickle.load(f)
-    app.run(debug=True)
+        twt_model = pickle.load(f)
+    with open('insta_model_pkl', 'rb') as i:
+        ig_model = pickle.load(i)
+    
+    app.run(debug=True, port=3000)
